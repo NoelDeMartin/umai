69d3ebcbe3572148fb734794908a9411d7497fa0
Noel De Martin <noeldemartin@gmail.com>
2020-12-09 07:08:17 +0100

    Configure github pages deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,11 +9,15 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# prepare github pages
+echo "umai.noeldemartin.com" > CNAME
+touch .nojekyll
+
+# publish
 git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
-
 git push -f git@github.com:noeldemartin/umai.git main:gh-pages
 
 cd -
