53a5487e3247fc07101e6f2148d49e05bb25c62b
Noel De Martin <noeldemartin@gmail.com>
2021-03-06 18:16:51 +0100

    Use external eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,3 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        '@noeldemartin/eslint-config-typescript',
-        'plugin:vue/vue3-recommended',
-    ],
-    rules: {
-        'vue/html-indent': ['error', 4],
-        'vue/max-attributes-per-line': ['error', { singleline: 3 }],
-        'vue/max-len': ['error', {
-            code: 120,
-            ignoreHTMLAttributeValues: true,
-        }],
-        'max-len': 'off',
-    },
+    extends: ['@noeldemartin/eslint-config-vue'],
 };
