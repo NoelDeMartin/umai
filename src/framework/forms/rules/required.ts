62aa559c4b51cb3f2b9e19b52bf4e3ab3effcd22
Noel De Martin <noel@noeldemartin.com>
2023-02-10 18:35:26 +0100

    Improve required form rule

--- src/framework/forms/rules/required.ts
+++ src/framework/forms/rules/required.ts
@@ -3,8 +3,13 @@ import { translate } from '@/framework/utils/translate';
 import { defineValidationRule } from './';
 
 export default defineValidationRule(value => {
-    if (value)
+    if (typeof value === 'string' && value.trim().length > 0) {
         return null;
+    }
+
+    if (typeof value !== 'string' && value) {
+        return null;
+    }
 
     return translate('validation.required');
 });
