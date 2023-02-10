9e72d83c4345cf1ad0d5fc47d46d12ee56279435
Noel De Martin <noel@noeldemartin.com>
2023-02-10 10:34:13 +0100

    Refactor route components to Composition API

--- src/framework/routing/router/index.ts
+++ src/framework/routing/router/index.ts
@@ -1,12 +1,13 @@
 import { applyMixins, fail, tap } from '@noeldemartin/utils';
-import { createRouter } from 'vue-router';
-import { defineComponent, h } from 'vue';
+import { createRouter, onBeforeRouteLeave, onBeforeRouteUpdate, useRoute } from 'vue-router';
+import { defineComponent, h, onUnmounted, ref } from 'vue';
 import type { Component, ConcreteComponent } from 'vue';
 import type { Constructor } from '@noeldemartin/utils';
-import type { RouteMeta, RouteRecordRaw, Router, RouterOptions } from 'vue-router';
+import type { RouteMeta, RouteRecordRaw, RouterOptions , Router as VueRouter } from 'vue-router';
 
 import Browser from '@/framework/core/facades/Browser';
 import FrameworkRouter from '@/framework/routing/router/FrameworkRouter';
+import Router from '@/framework/core/facades/Router';
 import UI from '@/framework/core/facades/UI';
 import { ApplicationComponent } from '@/framework/core/services/UIService';
 import { translate } from '@/framework/utils/translate';
@@ -34,120 +35,104 @@ function enhanceRoute(route: RouteRecordRaw): RouteRecordRaw {
     return route;
 }
 
-function enhanceRouter(router: Router): Router {
-    applyMixins(router.constructor as Constructor<Router>, [FrameworkRouter]);
+function enhanceRouter(router: VueRouter): VueRouter {
+    applyMixins(router.constructor as Constructor<VueRouter>, [FrameworkRouter]);
 
     return tap(router, router => router.initialize());
 }
 
-function callMethod<T extends string>(
-    target: any, // eslint-disable-line @typescript-eslint/no-explicit-any
-    method: T,
-    scope: unknown,
-    args: unknown[],
-) {
-    return target[method]?.call(scope, ...args);
+function enhancedRouteComponentSetup(pageComponent: ConcreteComponent, meta?: RouteMeta) {
+    const route = useRoute();
+    const subscriptions = ref<Function[]>([]);
+    const routeParameters = ref<Record<string, unknown> | null>(null);
+
+    function updateRouteParameters() {
+        routeParameters.value = resolveRouteParameters();
+
+        if (!routeParameters.value) {
+            UI.hideHeader();
+        }
+    }
+
+    function resolveRouteParameters(): Record<string, unknown> | null {
+        try {
+            return Object
+                .entries(route.params)
+                .reduce(
+                    (params, [paramName, paramValue]) => {
+                        if (Array.isArray(paramValue)) {
+                            return params;
+                        }
+
+                        const modelBinding = Router.getModelBinding(paramName);
+
+                        if (!modelBinding)
+                            return params;
+
+                        return tap(params, params => {
+                            params[paramName] = modelBinding.find(paramValue) ?? fail();
+
+                            if (modelBinding.subscribe)
+                                subscriptions.value.push(modelBinding.subscribe(paramValue, model => {
+                                    if (!routeParameters.value) {
+                                        return;
+                                    }
+
+                                    if (!model) {
+                                        routeParameters.value = null;
+
+                                        return;
+                                    }
+
+                                    routeParameters.value[paramName] = new Proxy(model, {});
+                                }));
+                        });
+                    },
+                    Object.assign({}, route.params) as Record<string, unknown>,
+                );
+        } catch (error) {
+            return null;
+        }
+    }
+
+    onBeforeRouteLeave(() => UI.showHeader());
+    onBeforeRouteUpdate(() => updateRouteParameters());
+    onUnmounted(() => {
+        subscriptions.value.forEach(unsubscribe => unsubscribe());
+        subscriptions.value = [];
+    });
+
+    updateRouteParameters();
+
+    return () => {
+        const requiredIndexedDB = meta?.requiresIndexedDB ?? true;
+
+        if (requiredIndexedDB && !Browser.supportsIndexedDB) {
+            const report: ErrorReport = {
+                title: translate('errors.unsupportedBrowser'),
+                description: translate('errors.unsupportedBrowser_indexedDB'),
+            };
+
+            return h(UI.resolveComponent<ConcreteComponent>(ApplicationComponent.ErrorReport), { report });
+        }
+
+        if (!routeParameters.value) {
+            return h(UI.resolveComponent(ApplicationComponent.NotFound));
+        }
+
+        return h(pageComponent, routeParameters.value);
+    };
 }
 
 function enhanceRouteComponent(pageComponent: ConcreteComponent, meta?: RouteMeta): Component {
     return defineComponent({
-        data: () => ({
-            subscriptions: [] as Function[],
-            routeParameters: null as Record<string, unknown> | null,
-        }),
-        created() {
-            this.updateRouteParameters();
-        },
-        unmounted() {
-            this.clearSubscriptions();
-        },
-        beforeRouteEnter(...args) {
-            return callMethod(pageComponent, 'beforeRouteEnter', this, args);
-        },
-        beforeRouteUpdate(...args) {
-            this.updateRouteParameters();
-
-            return callMethod(pageComponent, 'beforeRouteUpdate', this, args);
-        },
-        beforeRouteLeave(...args) {
-            UI.showHeader();
-
-            return callMethod(pageComponent, 'beforeRouteLeave', this, args);
-        },
-        methods: {
-            clearSubscriptions() {
-                this.subscriptions.forEach(unsubscribe => unsubscribe());
-                this.subscriptions = [];
-            },
-            updateRouteParameters(): void {
-                this.routeParameters = this.resolveRouteParameters();
-
-                if (!this.routeParameters) {
-                    UI.hideHeader();
-                }
-            },
-            resolveRouteParameters(): Record<string, unknown> | null {
-                try {
-                    return Object
-                        .entries(this.$route.params)
-                        .reduce(
-                            (params, [paramName, paramValue]) => {
-                                if (Array.isArray(paramValue)) {
-                                    return params;
-                                }
-
-                                const modelBinding = this.$router.getModelBinding(paramName);
-
-                                if (!modelBinding)
-                                    return params;
-
-                                return tap(params, params => {
-                                    params[paramName] = modelBinding.find(paramValue) ?? fail();
-
-                                    if (modelBinding.subscribe)
-                                        this.subscriptions.push(modelBinding.subscribe(paramValue, model => {
-                                            if (!this.routeParameters)
-                                                return;
-
-                                            if (!model) {
-                                                this.routeParameters = null;
-
-                                                return;
-                                            }
-
-                                            this.routeParameters[paramName] = new Proxy(model, {});
-                                        }));
-                                });
-                            },
-                            Object.assign({}, this.$route.params) as Record<string, unknown>,
-                        );
-                } catch (error) {
-                    return null;
-                }
-            },
-        },
-        render() {
-            const requiredIndexedDB = meta?.requiresIndexedDB ?? true;
-
-            if (requiredIndexedDB && !Browser.supportsIndexedDB) {
-                const report: ErrorReport = {
-                    title: translate('errors.unsupportedBrowser'),
-                    description: translate('errors.unsupportedBrowser_indexedDB'),
-                };
-
-                return h(UI.resolveComponent<ConcreteComponent>(ApplicationComponent.ErrorReport), { report });
-            }
-
-            if (!this.routeParameters) {
-                return h(UI.resolveComponent(ApplicationComponent.NotFound));
-            }
-
-            return h(pageComponent, this.routeParameters);
+        setup() {
+            return enhancedRouteComponentSetup(pageComponent, meta);
         },
     });
 }
 
-export function createFrameworkRouter(options: RouterOptions): FrameworkRouter & Router {
+export function createFrameworkRouter(options: RouterOptions): FrameworkRouter & VueRouter {
     options.routes = options.routes.map(enhanceRoute);
 
     return enhanceRouter(createRouter(options));
